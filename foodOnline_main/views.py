7f9babb58f1732f0be1f35fd8ed8bff905ad67d1
Supriya Gaikwad <gaikwad.suupriya@gmail.com>
2025-09-04 11:29:26 +0530

    homepage setup,postgress configuration and .env-sample

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the Food Online Home Page")
+    return render(request, 'home.html')
